707c8207dd50f88e1dc539398185789a3a11dc66
Szan <weteran.xd250@gmail.com>
2025-08-24 00:46:04 +0200

    Dodanie Obrażeń od kolizji hitboxa nie tylko kiedy entity stoi na bloku block od teraz ma model miodu

--- src/main/java/net/szan/spectercraft/block/AcidBlock.java
+++ src/main/java/net/szan/spectercraft/block/AcidBlock.java
@@ -2,23 +2,33 @@ package net.szan.spectercraft.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.entity.damage.DamageSource;
 
 public class AcidBlock extends Block {
+
+    private static final VoxelShape SHAPE = Block.createCuboidShape(1, 0, 1, 15, 15, 15);
+
     public AcidBlock(Settings settings) {
         super(settings);
     }
-
+    //Miodowy Hitbox
+    public VoxelShape getOutlineShape(BlockState state , BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
     @Override
-    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        super.onSteppedOn(world, pos, state, entity);
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        super.onEntityCollision(state, world, pos, entity);
 
         if (!world.isClient && entity instanceof LivingEntity living) {
             living.damage(world.getDamageSources().magic(), 2.0F);
         }
     }
+
 }
