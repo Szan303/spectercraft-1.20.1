d4487a6482828bf7f23547bfacf3c50f1e170bbc
Szan <weteran.xd250@gmail.com>
2025-08-24 00:45:03 +0200

    Dodanie możliwości dodania przezroczystej powłoki na teksturze

--- src/main/java/net/szan/spectercraft/SpecterCraftClient.java
+++ src/main/java/net/szan/spectercraft/SpecterCraftClient.java
@@ -1,10 +1,14 @@
 package net.szan.spectercraft;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+
+import net.minecraft.client.render.RenderLayer;
+import net.szan.spectercraft.block.ModBlocks;
 
 public class SpecterCraftClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ACID, RenderLayer.getTranslucent());
     }
 }
