56daa45a993f426e65c5a98399327ef98c3ea5b0
Szan <96530860+Szan303@users.noreply.github.com>
2025-08-27 23:55:55 +0200

    Update SpecterCraft.java

--- src/main/java/net/szan/spectercraft/SpecterCraft.java
+++ src/main/java/net/szan/spectercraft/SpecterCraft.java
@@ -20,8 +20,4 @@ public class SpecterCraft implements ModInitializer {
 		ModBlocks.registerModBlocks();
 		LOGGER.info("Hello SpecterCraft!");
 	}
-    public void nigger(){
-        System.out.println("nagger");
-        System.out.println("nagger");
-    }
 }
